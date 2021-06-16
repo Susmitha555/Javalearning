414556cec0c45aa2a483e0a29af8c91ae3231755
emayavs <susmitha.emayavaramban@equiniti.com>
2021-06-16 20:16:54 +0100

    My first commit

---
+++ EXCEL/src/Excel.java
@@ -0,0 +1,19 @@
+import com.codoid.products.fillo.Connection;
+import com.codoid.products.fillo.Fillo;
+import com.codoid.products.fillo.Recordset;
+
+public class Excel {
+	
+	public static void main(String[] args) {
+		
+	/*	Fillo fl=new Fillo();
+		Connection rd=fl.getConnection("");*/
+		
+		String a="susmitha";
+		
+		System.out.println(a.indexOf(0));
+		
+		
+	}
+
+}
